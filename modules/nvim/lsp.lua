375d471198d6931d154434fa909de70cac4d047e
MakeShiftArtist <47653736+MakeShiftArtist@users.noreply.github.com>
2026-04-25 17:50:37 -0500

    feat(lsp): configure python ty(pe) checking

--- modules/nvim/lsp.lua
+++ modules/nvim/lsp.lua
@@ -108,7 +108,7 @@ vim.lsp.config("rust-analyzer", {
 })
 vim.lsp.enable("rust-analyzer")
 
--- Python
+-- Python linting
 vim.lsp.config("ruff", {
     cmd = { "ruff", "server" },
     filetypes = { "python" },
@@ -117,3 +117,17 @@ vim.lsp.config("ruff", {
 })
 vim.lsp.enable("ruff")
 
+-- Python type checking
+vim.lsp.config("ty", {
+    cmd = { "ty", "server" },
+    filetypes = { "python" },
+    root_markers = {
+        "ty.toml",
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        ".git",
+    },
+})
+vim.lsp.enable("ty")
